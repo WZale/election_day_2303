f35846738a052eb52e62b187b09a386821ff1b4f
William Zale <williamzale@gmail.com>
2023-04-14 11:22:30 -0400

    Add test for vote_for! method

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe do
     expect(diana.party).to eq(:democrat)
     expect(diana.votes).to eq(0)
   end
+
+  it 'has a vote_for! method' do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+    
+    3.times do 
+      diana.vote_for!
+    end
+    
+    expect(diana.votes).to eq(3)
+    
+    diana.vote_for!
+    expect(diana.votes).to eq(4)
+  end
 end
